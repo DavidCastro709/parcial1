eecad00207446a3772e0f27f60e2d66a2af2e1a7
DavidCastro709 <davidcastrosoto709@gmail.com>
2026-02-05 09:44:19 -0600

    Primer commit del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import mifoto from './s38.jpeg';
 
 function App() {
+  const [verDescargas, setVerDescargas] = useState(false);
+  const nombreAlumno = "Castro Soto David Alberto";
+
+  if (verDescargas) {
+    return (
+      <div className="pantalla-oscura">
+        <div className="centro-descargas-directo">
+          <h1 className="titulo-seccion">Centro de Descargas</h1>
+          <div className="lista-botones">
+            
+            {/* 1. Comandos React */}
+            <a href="/comandos.pdf" download="Comandos_React_Angel.pdf">
+              <button className="btn btn-azul">COMANDOS BÁSICOS DE REACT</button>
+            </a>
+
+            {/* 2. ISO / IEEE */}
+            <a href="/ieee.pdf" download="ISO_Estándar_IEEE_Angel.pdf">
+              <button className="btn btn-verde">ISO / ESTANDAR IEEE</button>
+            </a>
+
+            {/* 3. Requerimientos */}
+            <a href="/requerimientos.pdf" download="Requerimientos_Angel.pdf">
+              <button className="btn btn-gris">REQUERIMIENTOS FUNCIONALES Y NO FUNCIONALES</button>
+            </a>
+
+            {/* 4. Python SHA-256 */}
+            <a href="/sha_256.pdf" download="Codigo_Python_SHA256_Angel.pdf">
+              <button className="btn btn-gris">CÓDIGO PYTHON ALGORITMO SHA-256</button>
+            </a>
+
+            <button className="btn btn-gris" onClick={() => setVerDescargas(false)} style={{marginTop: '20px', border: '1px solid #555'}}>
+              VOLVER AL PERFIL
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pantalla-oscura">
+      <div className="contenedor-perfil">
+        <img src={mifoto} alt="Perfil" className="foto-perfil-simple" />
+        <h1 className="evaluacion-texto">EVALUACIÓN PARCIAL 1</h1>
+        <p className="alumno-texto">Alumno: {nombreAlumno}</p>
+        <div className="enlaces-inferiores">
+          <a href="https://www.linkedin.com/in/david-alberto-castro-soto-b62450351" target="_blank" rel="noreferrer">
+            Visitar mi perfil de Linkedin
+          </a>
+          <button onClick={() => setVerDescargas(true)} className="boton-enlace">
+            Documentación Parcial 1
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
